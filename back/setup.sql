290a94566345785fd5f5263365228cf8a550159c
Robert Silver <rls442@drexel.edu>
2025-02-15 10:26:30 -0500

    set up for repo

---
+++ back/setup.sql
@@ -0,0 +1,80 @@
+-- Users Table
+CREATE TABLE users ( 
+    id VARCHAR(50) PRIMARY KEY, 
+    email VARCHAR(50) NOT NULL, 
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(25) NOT NULL
+);
+
+-- DailyFood Table
+CREATE TABLE daily_food ( 
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    user_id VARCHAR(50) NOT NULL,
+    meal_type VARCHAR(50) NOT NULL, 
+    name VARCHAR(50) NOT NULL,
+    calories INTEGER NOT NULL,
+    carbs INTEGER NOT NULL,
+    fat INTEGER NOT NULL,
+    protein INTEGER NOT NULL,
+    sodium INTEGER NOT NULL,
+    sugar INTEGER NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- Meals Table
+CREATE TABLE meals ( 
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    date TEXT NOT NULL,  -- Use TEXT for dates in SQLite
+    saved_meal BOOLEAN NOT NULL DEFAULT 0
+);
+
+-- Meal Items Join Table (for list of food IDs in a meal)
+CREATE TABLE meal_items (
+    meal_id INTEGER NOT NULL,
+    food_id INTEGER NOT NULL,
+    PRIMARY KEY (meal_id, food_id),
+    FOREIGN KEY (meal_id) REFERENCES meals(id) ON DELETE CASCADE,
+    FOREIGN KEY (food_id) REFERENCES daily_food(id) ON DELETE CASCADE
+);
+
+-- Meal Plans Table
+CREATE TABLE meal_plans ( 
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    is_private BOOLEAN NOT NULL DEFAULT 1
+);
+
+-- Meal Plan Items Join Table (for list of meal IDs in a meal plan)
+CREATE TABLE meal_plan_items (
+    meal_plan_id INTEGER NOT NULL,
+    meal_id INTEGER NOT NULL,
+    PRIMARY KEY (meal_plan_id, meal_id),
+    FOREIGN KEY (meal_plan_id) REFERENCES meal_plans(id) ON DELETE CASCADE,
+    FOREIGN KEY (meal_id) REFERENCES meals(id) ON DELETE CASCADE
+);
+
+-- Workout Table
+CREATE TABLE workouts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name VARCHAR(255) NOT NULL,
+    date TEXT NOT NULL
+);
+
+-- Exercises Table
+CREATE TABLE exercises (  
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name_of_workout VARCHAR(255) NOT NULL,
+    muscle_worked VARCHAR(255),
+    duration INTEGER, -- duration in minutes
+    sets INTEGER,
+    reps INTEGER,
+    weight INTEGER
+);
+
+-- Workout Exercises Join Table (for list of exercise IDs in a workout)
+CREATE TABLE workout_exercises (
+    workout_id INTEGER NOT NULL,
+    exercise_id INTEGER NOT NULL,
+    PRIMARY KEY (workout_id, exercise_id),
+    FOREIGN KEY (workout_id) REFERENCES workouts(id) ON DELETE CASCADE,
+    FOREIGN KEY (exercise_id) REFERENCES exercises(id) ON DELETE CASCADE
+);
