d303df07ff6c609e15aa45d3d5675fb97c53475e
BryanChan29 <mr.bryanchan@gmail.com>
2025-02-15 10:34:08 -0500

    setup with basic files

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,15 +1,15 @@
-import express, { Response } from "express";
-import sqlite3 from "sqlite3";
-import { open } from "sqlite";
-import * as url from "url";
+import express, { Response } from 'express';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import * as url from 'url';
 
 let app = express();
 app.use(express.json());
 
 // run server
 let port = 3000;
-let host = "localhost";
-let protocol = "http";
+let host = 'localhost';
+let protocol = 'http';
 app.listen(port, host, () => {
-    console.log(`${protocol}://${host}:${port}`);
+  console.log(`${protocol}://${host}:${port}`);
 });
