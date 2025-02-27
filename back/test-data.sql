2154b3a6163336727a59d72be24ecf86d4862fdd
BryanChan29 <mr.bryanchan@gmail.com>
2025-02-27 10:36:00 -0500

    Update test-data for hashed passwords and null values

--- back/test-data.sql
+++ back/test-data.sql
@@ -1,6 +1,8 @@
-INSERT INTO users (id, email, username, password) VALUES
-('user1', 'alice@example.com', 'alice123', 'password1'),
-('user2', 'bob@example.com', 'bob456', 'password2');
+-- alice@example.com:password
+-- bob@example.com:password
+INSERT INTO users (id, email, username, password, current_weight, goal_weight, height, age, activity_level) VALUES
+('user1', 'alice@example.com', 'alice123', '$argon2id$v=19$m=65536,t=3,p=4$iXgrKMu5e9OLt3hbMh568A$fx1QqmmsKO8O124zzHB3/9ayTdvjnGffpN7NDp3UEEY', null, null, null, null, null),
+('user2', 'bob@example.com', 'bob456', '$argon2id$v=19$m=65536,t=3,p=4$iXgrKMu5e9OLt3hbMh568A$fx1QqmmsKO8O124zzHB3/9ayTdvjnGffpN7NDp3UEEY', null, null, null, null, null);
 
 INSERT INTO daily_food (id, user_id, meal_type, name, calories, carbs, fat, protein, sodium, sugar) VALUES
 (1, 'user1', 'dinner', 'Chicken Stir-fry with White rice (1 serving)', 507, 10, 14, 56, 1400, 3),
